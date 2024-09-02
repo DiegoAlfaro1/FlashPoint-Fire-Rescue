8d9abcdb7a8a96ca845df3ee7e54c0d94fb45680
Rommel <rommtoledo@gmail.com>
2024-09-02 09:08:19 -0600

    Update file to match Unity proposed changes

--- Flash Point - Fire Rescue (Visualization)/Assets/Scripts/Services/HTTPService.cs
+++ Flash Point - Fire Rescue (Visualization)/Assets/Scripts/Services/HTTPService.cs
@@ -8,31 +8,29 @@ public class HTTPService : MonoBehaviour
     private const string serverUrl = "http://localhost:5000";
 
     // Method for starting the game
-    public IEnumerator StartGame(string filePath, int nAgents, Action<string> callback)
+    public IEnumerator StartGame(string filePath, int numAgents, System.Action<string> callback)
     {
-        // Create the JSON data for the request
-        string jsonData = JsonUtility.ToJson(new
-        {
-            file_path = filePath,
-            n_agents = nAgents
-        });
+        // Creates a WWWForm to build the POST request
+        WWWForm form = new WWWForm();
+        form.AddField("filePath", filePath);
+        form.AddField("numAgents", numAgents);
 
-        // Create the request
-        // TODO: Evaluate if it's necessary to use PostWwwForm
-        using (UnityWebRequest request = UnityWebRequest.Post(serverUrl + "/start_game", jsonData))
+        // Crea la solicitud POST usando UnityWebRequest.PostWwwForm
+        using (UnityWebRequest www = UnityWebRequest.Post(serverUrl + "/start_game", form))
         {
-            request.SetRequestHeader("Content-Type", "application/json");
-            
-            yield return request.SendWebRequest();
+            Debug.Log(www.url);
+            yield return www.SendWebRequest();
 
-            if (request.result == UnityWebRequest.Result.Success)
+            if (www.result != UnityWebRequest.Result.Success)
             {
-                string responseText = request.downloadHandler.text;
-                callback?.Invoke(responseText);
+                Debug.LogError("Error at initializing the game: " + www.error);
+                Debug.LogError("Response Code: " + www.responseCode);
+                Debug.LogError("Response Text: " + www.downloadHandler.text);
             }
             else
             {
-                Debug.LogError("Error at initializing the game: " + request.error);
+                Debug.Log("Game started successfully: " + www.downloadHandler.text);
+                callback?.Invoke(www.downloadHandler.text);
             }
         }
     }
