d39786afcefbd41ff01fc5b46d3b1a4f339b30b2
Rommel <rommtoledo@gmail.com>
2024-09-03 13:32:43 -0600

    Use same data structures use in python simulation

--- Flash Point - Fire Rescue (Visualization)/Assets/Scripts/Model/GameState.cs
+++ Flash Point - Fire Rescue (Visualization)/Assets/Scripts/Model/GameState.cs
@@ -5,43 +5,43 @@ using UnityEngine;
 [Serializable]
 public class GameState
 {
-    public int Step;
-    public List<List<int>> GridStructure;
-    public List<List<int>> OutOfBoundsGridStructure;
-    public int DamageMarkers;
-    public int RescuedVictims;
-    public int LostVictims;
-    public bool Running;
-    public int AgentCount;
-    public List<Vector2> FireLocations;
-    public List<Vector2> SmokeLocations;
-    public List<POI> PoiLocations;
-    public List<Firefighter> FirefighterPositions;
+    public int step;
+    public Dictionary<string, List<List<Vector2>>> grid_structure;
+    public Dictionary<string, List<List<Vector2>>> out_of_bounds_grid_structure;
+    public int damage_markers;
+    public int rescued_victims;
+    public int lost_victims;
+    public bool running;
+    public int agent_count;
+    public List<Vector2> fire_locations;
+    public List<Vector2> smoke_locations;
+    public List<POI> poi_locations;
+    public List<Firefighter> firefighter_positions;
 
     public GameState()
     {
-        GridStructure = new List<List<int>>();
-        OutOfBoundsGridStructure = new List<List<int>>();
-        FireLocations = new List<Vector2>();
-        SmokeLocations = new List<Vector2>();
-        PoiLocations = new List<POI>();
-        FirefighterPositions = new List<Firefighter>();
+        // Initialize the data structures
+        grid_structure = new Dictionary<string, List<List<Vector2>>>();
+        out_of_bounds_grid_structure = new Dictionary<string, List<List<Vector2>>>();
+        fire_locations = new List<Vector2>();
+        smoke_locations = new List<Vector2>();
+        poi_locations = new List<POI>();
+        firefighter_positions = new List<Firefighter>();
     }
 }
 
-// Auxiliar Classes for Other Game Objects
-
+// Auxiliary classes for the GameState
 [Serializable]
 public class POI
 {
-    public Vector2 Position;
-    public bool Revealed;
+    public Vector2 position;
+    public bool revealed;
 }
 
 [Serializable]
 public class Firefighter
 {
-    public int Id;
-    public Vector2 Position;
-    public bool CarryingVictim;
+    public int id;
+    public Vector2 position;
+    public bool carrying_victim;
 }
